33a038b3c782cb746b78c54d08029ec841c6c20e
Aryan Bhatt <you@example.com>
2019-08-07 12:54:16 -0400

    added 2013E

--- 2011C/sol.py
+++ 2011C/sol.py
@@ -1,19 +1,4 @@
 file = open("in.txt", 'r')
 lineArr = file.readlines()
-#for line in lineArr:
-#	print(line)
-
-def fact(n):
-	if n==0:
-		return 1
-	return n*fact(n-1)
-
-def f(n, k):
-	if k==0:
-		return fact(n)
-	return f(n, k-1) - f(n-1, k-1)
-
-print(f(4,1))
-for i in range(1,18):
-	for j in range(1, i+1):
-		print(i, j, f(i,j))
+for line in lineArr:
+	print(line)
